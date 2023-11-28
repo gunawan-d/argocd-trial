954e190de5582cf33caa12628fb987781e7140ce
Gunawan <gunawandwi255@gmail.com>
2023-11-28 10:51:11 +0700

    devops - add ingress on manifests v3

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home: NextPage = () => {
   return (
     <div className="h-full">
       <Head>
-        <title>ArgoCD</title>
+        <title>ArgoCD V3</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
